e926a0997bfdef9011486ff1179eff89ec36f24f
Matt Pruitt <guitsaru@gmail.com>
2009-06-17 12:48:41 -0500

    Added fake web responses.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,16 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
+require 'fakeweb'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'scraper'
 
 class Test::Unit::TestCase
+  FakeWeb.register_uri(:get, "http://example.com/main.html", :file => File.join(File.dirname(__FILE__), 'fake_pages/main.html'))
+  FakeWeb.register_uri(:get, "http://example.com/first_page.html", :file => File.join(File.dirname(__FILE__), 'fake_pages/first_page.html'))
+  FakeWeb.register_uri(:get, "http://example.com/first_child_page.html", :file => File.join(File.dirname(__FILE__), 'fake_pages/first_child_page.html'))
+  FakeWeb.register_uri(:get, "http://example.com/not_added.html", :file => File.join(File.dirname(__FILE__), 'fake_pages/not_added.html'))
 end
+
